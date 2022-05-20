2be668fa09f77b4547cb0e1b57ee3cc130bda1f9
arthurvini007 <88783140+arthurvini007@users.noreply.github.com>
2022-05-20 16:44:53 -0300

    Add files via upload

---
+++ Hotel/src/classes/HOTEL.java
@@ -0,0 +1,34 @@
+package classes;
+
+public class HOTEL {
+   private int qtdQuartos;
+   private String nome;
+   private String email;
+   private boolean vaga ;
+
+// Construtores Simples 
+public int getQtdQuartos() {
+    return qtdQuartos = 10;
+}
+public void setQtdQuartos(int qtdQuartos) {
+    this.qtdQuartos = qtdQuartos;
+}
+public String getNome() {
+    return nome;
+}
+public void setNome(String nome) {
+    this.nome = nome;
+}
+public String getEmail() {
+    return email;
+}
+public void setEmail(String email) {
+    this.email = email;
+}
+public boolean isVaga() {
+    return vaga;
+}
+public void setVaga(boolean vaga) {
+    this.vaga = vaga;
+}
+}
